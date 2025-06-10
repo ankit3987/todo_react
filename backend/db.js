42ec689d4a435dce7d3b40131eaa9d536f34c929
Ubuntu <ubuntu@ip-172-31-6-68.ap-south-1.compute.internal>
2025-06-10 11:11:10 +0000

    update-dockerfile

--- backend/db.js
+++ backend/db.js
@@ -1,20 +1,39 @@
-require("dotenv").config();
-const mongoose = require("mongoose");
+//require("dotenv").config();
+//const mongoose = require("mongoose");
 
-const uri = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+//const uri = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 //const uri = process.env.MONGO_URL;
 
+//const connectDB = async () => {
+//  try {
+//    await mongoose.connect(uri, {
+//      useNewUrlParser: true,
+//      useUnifiedTopology: true,
+//    });
+//    console.log("MongoDB connected");
+//  } catch (err) {
+//    console.error("MongoDB connection error:", err);
+//    process.exit(1);
+//  }
+//};
+
+//module.exports = connectDB;
+
+
+
+const mongoose = require("mongoose");
+require("dotenv").config();
+
 const connectDB = async () => {
-  try {
-    await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("MongoDB connected");
-  } catch (err) {
-    console.error("MongoDB connection error:", err);
-    process.exit(1);
-  }
+    try {
+        await mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error("MongoDB connection error:", error);
+    }
 };
 
 module.exports = connectDB;
