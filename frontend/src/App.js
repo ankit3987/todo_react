5d5ae25d9b441d2efa66ba4e09c409073820aa91
Ankit Rawat <138962370+ankit3987@users.noreply.github.com>
2025-06-26 01:06:18 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ class App extends Tasks {
         return (
             <div className="app">
                 <header className="app-header">
-                    <h1>My To-Do List  new code </h1>
+                    <h1>My To-Do List  new </h1>
                 </header>
                 <div className="main-content">
                     <Paper elevation={3} className="todo-container">
