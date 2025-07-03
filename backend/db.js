bda8c2e6d4d77c5aeb196c85930490609bdad3fd
ankit <ankit.rawat3987@gmail.com>
2025-07-04 04:31:42 +0530

    new changes in db.js

--- backend/db.js
+++ backend/db.js
@@ -4,6 +4,8 @@
 //const uri = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 //const uri = process.env.MONGO_URL;
 
+//mongodb://ankit:ankit@localhost:27017/myappdb
+
 //const connectDB = async () => {
 //  try {
 //    await mongoose.connect(uri, {
@@ -22,11 +24,14 @@
 
 require('dotenv').config();
 const mongoose = require("mongoose");
-//require("dotenv").config();
+require("dotenv").config();
+
+const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL, {
+//        await mongoose.connect(process.env.MONGO_URL, {
+        await mongoose.connect(uri, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
